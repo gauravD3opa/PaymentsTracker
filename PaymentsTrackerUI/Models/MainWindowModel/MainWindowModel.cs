fb444910c929b9699d92410fcea68b96dbd34fb7
Gaurav Deopa <gauravdeopa4@gmail.com>
2024-02-03 20:11:45 +0530

    added structure

---
+++ PaymentsTrackerUI/Models/MainWindowModel/MainWindowModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentsTrackerUI.Models.MainWindowModel
+{
+    internal class MainWindowModel
+    {
+        public List<User> usersList = new List<User>();
+
+    }
+
+    internal class User
+    {
+        private string name;
+
+        string Name { get => name; set => name = value; }
+
+    }
+}
