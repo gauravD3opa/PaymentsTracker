0cb88a3470070a5660359bcb6264b0ea6a8d8048
Gaurav Deopa <gauravdeopa4@gmail.com>
2024-02-10 09:43:18 +0530

    changed structure

--- Services/PaymentsTrackerServices/UserService/IUserService.cs
+++ Services/PaymentsTrackerServices/UserService/IUserService.cs
@@ -4,15 +4,14 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
-using System.ServiceModel.Primitives;
 
 namespace PamymentsTrackerServices.UserService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUserService" in both code and config file together.
-    [ServiceContract]
+   
     public interface IUserService
     {
-        [OperationContract]
+        
         void DoWork();
     }
 }
